2b0a06647507eba6ccb410c6e584ba6791932b1a
Lari <larileal@hotmail.com.br>
2023-02-15 16:42:49 -0300

    Treino do tipo Delegate

---
+++ ExercicioDelegate/ExercicioDelegate/Program.cs
@@ -0,0 +1,45 @@
+﻿
+using ExercicioDelegate;
+using System.Diagnostics;
+
+namespace treino
+{
+    class Program
+    {
+        delegate double Operation(double numberToConvert);
+
+        static void Main(string[] args)
+        {
+            TemperatureConverter calc = new TemperatureConverter();
+            Operation operation;
+
+            Console.WriteLine("\r\n _____                                           \r\n/  __ \\                                          \r\n| /  \\/ ___  _ ____   _____ _ __ ___  ___  _ __  \r\n| |    / _ \\| '_ \\ \\ / / _ \\ '__/ __|/ _ \\| '__| \r\n| \\__/\\ (_) | | | \\ V /  __/ |  \\__ \\ (_) | |    \r\n \\____/\\___/|_| |_|\\_/ \\___|_|  |___/\\___/|_|    \r\n                                                 \r\n                                                 \r\n");
+            Console.WriteLine();
+            Console.WriteLine(" 1 - Celsius para Fahrenheit");
+            Console.WriteLine(" 2 - Fahrenheit para Celsius");
+            Console.Write(" Digite o número da opção: ");
+
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    operation = new Operation(calc.CelsiusToFahrenheit);
+                    Console.WriteLine();
+                    Console.Write(" Informe a temperatura em ºC: ");
+                    double celsius = double.Parse(Console.ReadLine());
+                    double resultadoEmFahrenheit = operation(celsius);
+                    Console.WriteLine($"O resultado é: {resultadoEmFahrenheit}");
+                    break;
+                case 2:
+                    operation = new Operation(calc.FahrenheitToCelsius);
+                    Console.WriteLine();
+                    Console.Write(" Informe a temperatura em ºF: ");
+                    double fahrenheit = double.Parse(Console.ReadLine());
+                    double resultado = operation(fahrenheit);
+                    Console.WriteLine($"O resultado é: {resultado}");
+                    break;
+            }
+        }
+    }
+}
---
+++ ExercicioDelegate/ExercicioDelegate/TemperatureConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioDelegate
+{
+    public class TemperatureConverter
+    {
+
+        public double CelsiusToFahrenheit(double celsius)
+        {
+            //°F = (°C x 9 / 5) +32
+            double calc = (celsius * 9 / 5) + 32;
+            double Fahrenheit = calc;
+
+            return Fahrenheit;
+        }
+
+        public double FahrenheitToCelsius(double fahrenheit)
+        {
+            //°C = (°F - 32) x 5 / 9
+            double calc = (fahrenheit - 32) * 5 / 9;
+
+            double celsius = calc;
+            return celsius;
+        }
+
+     
+    }
+}
